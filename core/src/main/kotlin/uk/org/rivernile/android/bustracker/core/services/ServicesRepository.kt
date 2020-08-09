c7856176c2a73e0302bd67977fa2d71d24317b0b
Niall Scott <riverniall@gmail.com>
2020-08-09 22:03:44 +0100

    Added ServiceRepository and unit tests.

---
+++ core/src/main/kotlin/uk/org/rivernile/android/bustracker/core/services/ServicesRepository.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2020 Niall 'Rivernile' Scott
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors or contributors be held liable for
+ * any damages arising from the use of this software.
+ *
+ * The aforementioned copyright holder(s) hereby grant you a
+ * non-transferrable right to use this software for any purpose (including
+ * commercial applications), and to modify it and redistribute it, subject to
+ * the following conditions:
+ *
+ *  1. This notice may not be removed or altered from any file it appears in.
+ *
+ *  2. Any modifications made to this software, except those defined in
+ *     clause 3 of this agreement, must be released under this license, and
+ *     the source code of any modifications must be made available on a
+ *     publically accessible (and locateable) website, or sent to the
+ *     original author of this software.
+ *
+ *  3. Software modifications that do not alter the functionality of the
+ *     software but are simply adaptations to a specific environment are
+ *     exempt from clause 2.
+ *
+ */
+
+package uk.org.rivernile.android.bustracker.core.services
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import uk.org.rivernile.android.bustracker.core.database.busstop.daos.ServicesDao
+import uk.org.rivernile.android.bustracker.core.di.ForIoDispatcher
+import javax.inject.Inject
+
+/**
+ * This repository is used to access services data.
+ *
+ * @param servicesDao The DAO to access the services data store.
+ * @param ioDispatcher The [CoroutineDispatcher] to perform IO operations on.
+ * @author Niall Scott
+ */
+class ServicesRepository @Inject internal constructor(
+        private val servicesDao: ServicesDao,
+        @ForIoDispatcher private val ioDispatcher: CoroutineDispatcher) {
+
+    /**
+     * Get a [Flow] which returns a [Map] of service names to colours for the service, and will emit
+     * further items if the backing store changes.
+     *
+     * @param services The services to get colours for. If `null` or empty, colours for all known
+     * services will be returned.
+     * @return The [Flow] which emits the service-colour mapping.
+     */
+    @ExperimentalCoroutinesApi
+    fun getColoursForServicesFlow(services: Array<String>?)
+            : Flow<Map<String, Int>?> = callbackFlow {
+        val listener = object : ServicesDao.OnServicesChangedListener {
+            override fun onServicesChanged() {
+                launch {
+                    getAndSendColoursForServices(channel, services)
+                }
+            }
+        }
+
+        servicesDao.addOnServicesChangedListener(listener)
+        getAndSendColoursForServices(channel, services)
+
+        awaitClose {
+            servicesDao.removeOnServicesChangedListener(listener)
+        }
+    }
+
+    /**
+     * A suspended function which obtains the colours for services and then sends it to the given
+     * [channel].
+     *
+     * @param channel The [SendChannel] that emissions should be sent to.
+     * @param services The services to get colours for. If `null`, gets colours for all known
+     * services.
+     */
+    private suspend fun getAndSendColoursForServices(
+            channel: SendChannel<Map<String, Int>?>,
+            services: Array<String>?) = withContext(ioDispatcher) {
+        channel.send(servicesDao.getColoursForServices(services))
+    }
+}
---
+++ core/src/test/kotlin/uk/org/rivernile/android/bustracker/core/services/ServicesRepositoryTest.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright (C) 2020 Niall 'Rivernile' Scott
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors or contributors be held liable for
+ * any damages arising from the use of this software.
+ *
+ * The aforementioned copyright holder(s) hereby grant you a
+ * non-transferrable right to use this software for any purpose (including
+ * commercial applications), and to modify it and redistribute it, subject to
+ * the following conditions:
+ *
+ *  1. This notice may not be removed or altered from any file it appears in.
+ *
+ *  2. Any modifications made to this software, except those defined in
+ *     clause 3 of this agreement, must be released under this license, and
+ *     the source code of any modifications must be made available on a
+ *     publically accessible (and locateable) website, or sent to the
+ *     original author of this software.
+ *
+ *  3. Software modifications that do not alter the functionality of the
+ *     software but are simply adaptations to a specific environment are
+ *     exempt from clause 2.
+ *
+ */
+
+package uk.org.rivernile.android.bustracker.core.services
+
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import uk.org.rivernile.android.bustracker.core.database.busstop.daos.ServicesDao
+import uk.org.rivernile.android.bustracker.coroutines.MainCoroutineRule
+import uk.org.rivernile.android.bustracker.coroutines.test
+
+/**
+ * Tests for [ServicesRepository].
+ *
+ * @author Niall Scott
+ */
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
+class ServicesRepositoryTest {
+
+    @Rule
+    @JvmField
+    val coroutineRule = MainCoroutineRule()
+
+    @Mock
+    private lateinit var servicesDao: ServicesDao
+
+    private lateinit var repository: ServicesRepository
+
+    @Before
+    fun setUp() {
+        repository = ServicesRepository(servicesDao, coroutineRule.testDispatcher)
+    }
+
+    @Test
+    fun getColoursForServicesFlowGetsInitialValue() = coroutineRule.runBlockingTest {
+        val serviceColours = mapOf(
+                "1" to 0x000000,
+                "2" to 0xFFFFFF)
+        whenever(servicesDao.getColoursForServices(null))
+                .thenReturn(serviceColours)
+
+        val observer = repository.getColoursForServicesFlow(null).test(this)
+        observer.finish()
+
+        observer.assertValues(serviceColours)
+        verify(servicesDao)
+                .removeOnServicesChangedListener(any())
+    }
+
+    @Test
+    fun getColoursForServicesFlowResponseToChanges() = coroutineRule.runBlockingTest {
+        doAnswer {
+            it.getArgument<ServicesDao.OnServicesChangedListener>(0).let { listener ->
+                listener.onServicesChanged()
+                listener.onServicesChanged()
+            }
+        }.whenever(servicesDao).addOnServicesChangedListener(any())
+        val serviceColours1 = mapOf(
+                "1" to 0x000000,
+                "2" to 0xFFFFFF)
+        val serviceColours2 = mapOf(
+                "3" to 0xFF0000,
+                "4" to 0x00FF00)
+        val serviceColours3 = mapOf(
+                "5" to 0x0000FF)
+        whenever(servicesDao.getColoursForServices(null))
+                .thenReturn(serviceColours1, serviceColours2, serviceColours3)
+
+        val observer = repository.getColoursForServicesFlow(null).test(this)
+        observer.finish()
+
+        observer.assertValues(serviceColours1, serviceColours2, serviceColours3)
+        verify(servicesDao)
+                .removeOnServicesChangedListener(any())
+    }
+}
