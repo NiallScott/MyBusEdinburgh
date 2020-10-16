272f3428e368cfa47fee8cea4193d400d19329a1
Niall Scott <riverniall@gmail.com>
2020-10-16 21:19:40 +0100

    Re-wrote ProximityLimitationsDialogFragment and TimeLimitationsDialogFragment in Kotlin.

---
+++ app/src/main/kotlin/uk/org/rivernile/android/bustracker/ui/alerts/time/TimeLimitationsDialogFragment.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2020 Niall 'Rivernile' Scott
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty.  In no event will the authors or contributors be held liable for
+ * any damages arising from the use of this software.
+ *
+ * The aforementioned copyright holder(s) hereby grant you a
+ * non-transferrable right to use this software for any purpose (including
+ * commercial applications), and to modify it and redistribute it, subject to
+ * the following conditions:
+ *
+ *  1. This notice may not be removed or altered from any file it appears in.
+ *
+ *  2. Any modifications made to this software, except those defined in
+ *     clause 3 of this agreement, must be released under this license, and
+ *     the source code of any modifications must be made available on a
+ *     publically accessible (and locateable) website, or sent to the
+ *     original author of this software.
+ *
+ *  3. Software modifications that do not alter the functionality of the
+ *     software but are simply adaptations to a specific environment are
+ *     exempt from clause 2.
+ *
+ */
+
+package uk.org.rivernile.android.bustracker.ui.alerts.time
+
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import uk.org.rivernile.edinburghbustracker.android.R
+
+/**
+ * This [DialogFragment] shows the user some disclaimer text regarding the time alert feature in the
+ * application.
+ *
+ * @author Niall Scott
+ */
+class TimeLimitationsDialogFragment : DialogFragment() {
+
+    companion object {
+
+        /**
+         * Create a new instance of [TimeLimitationsDialogFragment].
+         *
+         * @return A new instance of [TimeLimitationsDialogFragment].
+         */
+        @JvmStatic // TODO: remove annotation when AddTimeAlertDialogFragment is converted.
+        fun newInstance() = TimeLimitationsDialogFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        isCancelable = true
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?) =
+            AlertDialog.Builder(requireContext())
+                    .setTitle(R.string.timelimitationsdialog_title)
+                    .setMessage(R.string.timelimitationsdialog_message)
+                    .setNegativeButton(R.string.close, null)
+                    .create()
+}
