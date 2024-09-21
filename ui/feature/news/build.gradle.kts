ab646b541501d1381ce5ed32a01e91e8243490b6
Niall Scott <riverniall@gmail.com>
2024-09-21 19:19:09 +0200

    Make the :ui:feature:news module compatible with Kotlin 2.0.

--- ui/feature/news/build.gradle.kts
+++ ui/feature/news/build.gradle.kts
@@ -27,6 +27,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose.compiler)
     alias(libs.plugins.kotlin.kapt)
     alias(libs.plugins.hilt)
 }
