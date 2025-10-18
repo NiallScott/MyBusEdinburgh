68154a0a20053cf660fa0fb0b2577b9671c5652f
Niall Scott <riverniall@gmail.com>
2025-10-19 00:18:05 +0100

    Only run baseline profiles on 3 recent versions of the platform.

--- macrobenchmark/app-baselineprofile/build.gradle.kts
+++ macrobenchmark/app-baselineprofile/build.gradle.kts
@@ -59,9 +59,6 @@ android {
 
 baselineProfile {
     managedDevices += listOf(
-        "pixel2api28",
-        "pixel2api29",
-        "pixel2api30",
         "pixel2api31",
         "pixel2api33",
         "pixel2api34"
